667af88b6ee9e91e3c324ae307d6fd53ec0f418e
chipete20202021 <chipete20212020@gmail.com>
2022-12-15 11:27:17 -0800

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,15 @@
 #include "main.h"
-/**
- * print_alphabet_x10 - print 10 times the alphabet, in lowercase
+
+/*
+ * File: 2-print_alphabet_x10.c
+ * Written by Tyrone
  */
 
-void print_alphabet_x10(void);
+
+/**
+ * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
+ */
+void print_alphabet_x10(void)
 {
 	int count = 0;
 	char letter;
@@ -12,5 +18,6 @@ void print_alphabet_x10(void);
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
-		_putchar('/n');
+		_putchar('\n');
 	}
+}
