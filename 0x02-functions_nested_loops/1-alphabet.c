d034bbb33269b523a3962d0008765bd41fb9c0c6
chipete20202021 <chipete20212020@gmail.com>
2022-12-15 11:10:15 -0800

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * print_alphabet - print all alphabelt in lowercase
-*/
+ * print_alphabet - print all alphabet in lowercase, followed by a new line.
+ */
 
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char letter;
 
